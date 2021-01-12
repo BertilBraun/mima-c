7202935370f5a72b414bcb56b9578b7a44835b06
Felix Muehlenberend <felix.muehlenberend@gmail.com>
2021-01-12 15:27:53 +0100

    AST string representation
    Human readable for debugging only

--- ast.py
+++ ast.py
@@ -10,20 +10,39 @@ class NodeType(Enum):
     MULTIPLY   = 4
     MODULO     = 5
 
+node_str_repr = {
+    NodeType.INTLITERAL : "num",
+    NodeType.PLUS       : "+",
+    NodeType.MINUS      : "-",
+    NodeType.DIVIDE     : "/",
+    NodeType.MULTIPLY   : "*",
+    NodeType.MODULO     : "%"
+}
+
 class Node(object):
     def __init__(self, node_type : NodeType):
         self.node_type = node_type
+        # an ORDERED list of all children if needed
+        self._children = []
+
+    def __repr__(self):
+        result = "[{}]: ".format(node_str_repr[self.node_type])
+        for child in self._children:
+            result += '\t|'.join(('\n' + "->: " + str(child).lstrip()).splitlines(True))
+        return result
 
 class UnaryNode(Node):
     def __init__(self, node_type : NodeType, node):
         super().__init__(node_type)
         self.node = node
+        self._children = [node]
 
 class BinaryNode(Node):
     def __init__(self, node_type : NodeType, left_node, right_node):
         super().__init__(node_type)
         self.left_node = left_node
         self.right_node = right_node
+        self._children = [left_node, right_node]
 
 class TenaryNode(Node):
     def __init__(self, node_type : NodeType, left_node, center_node, right_node):
@@ -31,20 +50,26 @@ class TenaryNode(Node):
         self.left_node = left_node
         self.right_node = right_node
         self.center_node = center_node
+        self._children = [left_node, right_node, center_node]
 
 class ValueNode(Node):
     def __init__(self, node_type, value):
         super().__init__(node_type)
         self.value = value
 
+    def __repr__(self):
+        return "[{}]: ({})".format(node_str_repr[self.node_type], self.value)
 
-# (+) -> INTLITERAL(5)
-#     -> INTLITERAL(3)
 
-# "5" = INTLIERAL(5)
-
-# tree = BinaryNode(NODE_ADDITION
-#                   , ValueNode(NODE_INTLITERAL, 5)
-#                   , ValueNode(NODE_INTLITERAL, 3))
-
-# tree = ValueNode(NODE_INTLITERAL, 5)
+if __name__ == "__main__":
+    ast =  BinaryNode(NodeType.PLUS,
+                        ValueNode(NodeType.INTLITERAL, 0),
+                        BinaryNode(NodeType.DIVIDE,
+                                    BinaryNode(NodeType.MULTIPLY,
+                                            ValueNode(NodeType.INTLITERAL, 1),
+                                            ValueNode(NodeType.INTLITERAL, 2)
+                                            ),
+                                    ValueNode(NodeType.INTLITERAL, 2)
+                                    )
+                        )
+    print(ast)
