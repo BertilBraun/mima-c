f779260b154cbdda333cdd22b5f6920459975115
OrtwIn <bertil.braun.private@gmail.com>
2021-01-20 17:33:55 +0100

    Compiler Setup

--- compiler/Compiler.cs
+++ compiler/Compiler.cs
@@ -20,9 +20,23 @@ namespace mima_c.compiler
             outputFile.WriteLine(command);
         }
 
+        private void CreateMimaHeader()
+        {
+            // Add StackPointer, FramePoiter, Registers, Stack etc. to start of File
+            // Add Call to Main here aswell, 
+            // Main call should be the first thing executed, when the mima program gets run
+        }
+
+        // AST might have to be replaced with a PreEvaluated Intermediate Program representation
+        //   With compile time known Values replaced 
         public Runnable Compile(AST node)
         {
+            CreateMimaHeader();
 
+            // Compile Here
+
+            // Setup Function jumps
+            // Create Statements in Function bodies etc.
 
             outputFile.Close();
             return new Runnable(fileToCompileTo);
